8e181f3ebf04af15bacfbd1fd5a88e335556af99
khoussngom <khoussn@gmail.com>
2025-10-24 02:38:33 +0000

    ok

--- database/seeders/SeedDemoData.php
+++ database/seeders/SeedDemoData.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class SeedDemoData extends Seeder
 {
