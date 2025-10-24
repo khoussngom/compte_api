074bba66a155b2ab78c0ba14bbe885869bf81538
khoussngom <khoussn@gmail.com>
2025-10-24 01:03:20 +0000

    ok

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Documentation Swagger UI (static)
+Route::get('/docs', function () {
+    $path = public_path('docs/index.html');
+    if (file_exists($path)) {
+        return response()->file($path);
+    }
+    abort(404);
+});
